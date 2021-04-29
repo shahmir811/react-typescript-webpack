ae37e48db46576e5da8834c5a7bf31ae16a7e66b
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2021-04-29 12:56:16 +0500

    Add some more webpack packages

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,11 +1,14 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
+const bundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 
 module.exports = {
-	mode: 'production',
-	devtool: 'source-map',
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env.name': JSON.stringify('shahmir811'),
-		}),
-	],
-};
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('shahmir811'),
+    }),
+    new bundleAnalyzerPlugin(),
+  ],
+}
