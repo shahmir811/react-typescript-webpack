b3320614d1c3f2d63e99b76dafcd981814a11ab0
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2021-04-29 12:41:48 +0500

    Added Linting and prettier formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-import './styles.css';
+import './styles.css'
 
 const App = () => {
-	return (
-		<div>
-			<h1>React Typescript Webpack Starter Template</h1>
-		</div>
-	);
-};
+  return (
+    <div>
+      <h1>React Typescript Webpack Starter Template</h1>
+    </div>
+  )
+}
 
-export default App;
+export default App
